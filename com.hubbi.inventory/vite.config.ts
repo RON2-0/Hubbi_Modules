7140507ce16507e6aacdd4bae799df3cd0e4aab6
Ronald Guevara <ronaldguevara999@gmail.com>
2026-01-02 00:35:19 -0600

    fix(inventory): compatibilidad con el core

--- com.hubbi.inventory/vite.config.ts
+++ com.hubbi.inventory/vite.config.ts
@@ -9,6 +9,9 @@ const __dirname = dirname(__filename);
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    define: {
+        'process.env': {}
+    },
     build: {
         lib: {
             entry: resolve(__dirname, 'src/index.tsx'),
